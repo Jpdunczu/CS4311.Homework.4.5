8f6e4f5aabbe4aa834de7e73df0098ee99ebb862
Jpdunczu <jd52981@gmail.com>
2017-07-27 10:42:55 -0700

    Initial Commit

---
+++ CS4311.Homework.4.5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS4311.Homework.4.5
+//
+//  Created by Joshua Duncan on 7/27/17.
+//  Copyright © 2017 Joshua Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
